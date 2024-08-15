39d4cea43f4f15831c99c5f697c4092ea65b9a17
Stone-WYD <1174872613@qq.com>
2024-08-15 16:14:51 +0800

    静态资源创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
+// createRouter 创建路由实例
+// createWebHistory：创建 history 的路由模式
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // path 和 component 对应关系的位置
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      {
+        path: "/",
+        component: Layout,
+        children: [
+          {
+            path: '',
+            component: Home
+          },
+          {
+            path: '/category',
+            component: Category
+          }
+        ]
+      },
+      {
+        path: "/login",
+        component: Login
+      }
   ]
 })
 
