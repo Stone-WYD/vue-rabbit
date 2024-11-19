6727494ee8bb54fd5b0e6bb2beb5d9f1fe1bfca8
Stone-WYD <1174872613@qq.com>
2024-11-19 10:31:39 +0800

    二级目录，列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -23,14 +23,31 @@ const reqData = ref({
   pageSize: 20,
   sortField: 'publishTime'
 })
-const getgoodsList = async () => {
+const getGoodsList = async () => {
   const res = await getSubCategoryAPI(reqData.value)
-  console.log(res)
   goodsList.value = res.result.items
 } 
 
-onMounted(() => getgoodsList())
+onMounted(() => getGoodsList())
 
+// tab 切换回调
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodsList()
+}
+
+const disable = ref(false)
+// 加载更多数据
+const load = async () => {
+  // 获取下一页数据
+  reqData.value.page ++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodsList.value = [...goodsList.value,...res.result.items]
+  // 全部加载完，停止监听，不再调用接口
+  if(res.result.items.length === 0) {
+    disable.value = true
+  }
+}
 </script>
 
 <template>
@@ -45,12 +62,12 @@ onMounted(() => getgoodsList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disable">
          <!-- 商品列表-->
         <GoodsItem v-for="goods in goodsList" :goods="goods" :key="goods.id"/>
       </div>
