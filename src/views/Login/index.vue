6f7c9d24e98145e97301e01c2dd3fd7909507690
Stone-WYD <1174872613@qq.com>
2024-12-02 10:59:40 +0800

    pinia保存用户信息

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import { ref } from 'vue';
-import {loginAPI} from '@/apis/user'
-
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import {useRouter} from 'vue-router'
+
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
 
   // 表单校验（账户名+密码）
   // 1. 准备表单对象
@@ -49,8 +51,7 @@ import {useRouter} from 'vue-router'
       // 以 valid 为判断条件，如果通过校验才执行登录逻辑
       if(valid) {
         // TODO LOGIN
-        const res = await loginAPI({account, password})
-        console.log(res)
+        await userStore.getUserInfo({account, password})
         // 1. 提示用户
         ElMessage({type:'success', message:'登录成功'})
         // 2. 跳转到首页
